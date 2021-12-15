9a10df7c8df5db729d74139d242e112e71c1e1e0
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-15 13:02:23 +0500

    Control Flow

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,62 @@
-console.log(1);
-console.log(2);
+// for (let i = 0; i < 5; i++) {
+//   console.log("in loop:", i);
+// }
+
+// const names = ["Jahongir", "Aziz", "Otabek"];
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// const ninjas = ["a", "b", "c", "d"];
+
+// if (ninjas.length > 3) {
+//   console.log(" there are many ninjas ");
+// }
+
+// const password = "pass";
+
+// if (password.length >= 8) {
+//   console.log("your password is long enough");
+// } else {
+//   console.log(" Your password must contain at least 8 characters");
+// }
+
+// const scores = [10, 20, 30, 0, 40, 100, 80, 70];
+
+// for (let i = 0; i < scores.length; i++) {
+//   if (scores[i] === 0) {
+//     console.log("What a lame score you have");
+//     continue;
+//   }
+
+//   console.log("Your score is:", scores[i]);
+
+//   if (scores[i] === 100) {
+//     console.log(" You nerd reached the top score");
+//     break;
+//   }
+// }
+// const grade = "E";
+
+// switch (grade) {
+//   case "A":
+//     console.log("You got an A");
+//     break;
+//   case "B":
+//     console.log("You got an B");
+//     break;
+//   case "C":
+//     console.log("You got an C");
+//     break;
+//   case "D":
+//     console.log("You got an D");
+//     break;
+//   case "F":
+//     console.log("You got an F");
+//     break;
+//   default:
+//     console.log("You did not attend the exam absentee");
+// }
+
+
