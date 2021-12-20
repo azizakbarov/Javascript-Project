ab1313a0b6b69e2d12adbf518a9f574987206321
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-20 22:06:09 +0500

    The DOM model

--- sandbox.js
+++ sandbox.js
@@ -1,48 +1,8 @@
-// const speak = function (name, time) {
-//   console.log(`Good ${time} ${name}`);
-// };
+const para = document.querySelector("p");
 
-// speak("Aziz", "morning");
+const paras = document.querySelectorAll("p");
 
-// // const calcArea = function (radius) {
-// //   return 3.14 * radius ** 2;
-// // };
-
-// const calcArea = (radius) => {
-//   return 3.14 * radius ** 2;
-// };
-
-// const area = calcArea(5);
-// console.log("area is:", area);
-
-// const bill = (products, tax) => {
-//   let total = 0;
-//   for (let i = 0; i < products.length; i++) {
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// };
-
-// console.log(bill([10, 15, 30], 0.2));
-
-// const name = "Aziz";
-
-// const greet = () => "hello";
-// let resultOne = greet();
-// console.log(resultOne);
-
-// let resultTwo = name.toUpperCase();
-// console.log(resultTwo);
-
-// Math Object
-
-console.log(Math);
-console.log(Math.PI);
-
-const area = 7.7;
-console.log(Math.round(area));
-
-//Random Number 
-
-const random = Math.random();
-console.log(random)
+paras.forEach((para) => {
+  para.innerText += "new text";
+  console.log(para.innerText);
+});
