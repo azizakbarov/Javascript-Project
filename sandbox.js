e8cda442271d1084b7b4c4888fcafbf2f9bc1472
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-23 11:22:19 +0500

    Basic Form Validation

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,27 @@
 const form = document.querySelector(".signup-form");
+const feedback = document.querySelector(".feedback");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(form.username.value);
+  const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,}$/;
+
+  if (usernamePattern.test(username)) {
+    feedback.textContent = "username is valid";
+  } else {
+    feedback.textContent =
+      " username must be letters only & must contain more than 6 characters";
+  }
 });
 
-const username = "azizbek";
+// const username = "azizbek";
 
-const pattern = /^[a-z]{6,}$/;
+// const pattern = /^[a-z]{6,}$/;
 
-let result = pattern.test(username);
+// let result = pattern.test(username);
 
-if (result) {
-  console.log("Regex test is successful");
-} else {
-  console.log("Regex test failed");
-}
+// if (result) {
+//   console.log("Regex test is successful");
+// } else {
+//   console.log("Regex test failed");
+// }
