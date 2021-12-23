10cb5591e024a2615299f57ef3d1c390f0f28dc7
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-23 09:49:33 +0500

    More Events

--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,12 @@
-// const button = document.querySelector("button");
+const copy = document.querySelector(".copy-me");
 
-// button.addEventListener("click", () => {
-//   console.log("You clicked me!");
-// });
-
-const ul = document.querySelector("ul");
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-  //   ul.innerHTML += "<li> something new </li>";
-  const li = document.createElement("li");
-  li.textContent = "something new todo";
-  //   ul.append(li);
-  ul.prepend(li);
+copy.addEventListener("copy", () => {
+  console.log("OI! My content is a copyright!");
 });
 
-// const items = document.querySelectorAll("li");
+const box = document.querySelector(".box");
 
-// items.forEach((item) => {
-//   item.addEventListener("click", (e) => {
-//     // console.log("item clicked");
-//     // e.target.style.textDecoration = "line-through";
-//     e.stopPropagation();
-//     e.target.remove();
-//   });
-// });
-
-ul.addEventListener("click", (e) => {
-  // console.log("Event in UL ")
-  //   console.log(e.target);
-  if (e.target.tagName === "LI") {
-    e.target.remove();
-  }
+box.addEventListener("mousemove", (e) => {
+  //console.log(e)
+  box.textContent = ` x pos - ${e.offsetX}  y pos - ${e.offsetY}`;
 });
