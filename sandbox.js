647aececbb9d35f2185c562a33034c4c6954649c
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-23 11:08:30 +0500

    Testing REGEX patterns

--- sandbox.js
+++ sandbox.js
@@ -4,3 +4,15 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log(form.username.value);
 });
+
+const username = "azizbek";
+
+const pattern = /^[a-z]{6,}$/;
+
+let result = pattern.test(username);
+
+if (result) {
+  console.log("Regex test is successful");
+} else {
+  console.log("Regex test failed");
+}
