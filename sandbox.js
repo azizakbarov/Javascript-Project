5816db593a5a218fd8f407e31ccb90560c8cc6bd
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-23 08:50:09 +0500

    Creeating and removing elements

--- sandbox.js
+++ sandbox.js
@@ -4,11 +4,24 @@
 //   console.log("You clicked me!");
 // });
 
+const ul = document.querySelector("ul");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  //   ul.innerHTML += "<li> something new </li>";
+  const li = document.createElement("li");
+  li.textContent = "something new todo";
+  //   ul.append(li);
+  ul.prepend(li);
+});
+
 const items = document.querySelectorAll("li");
 
 items.forEach((item) => {
   item.addEventListener("click", (e) => {
     // console.log("item clicked");
-    e.target.style.textDecoration = "line-through";
+    // e.target.style.textDecoration = "line-through";
+    e.target.remove();
   });
 });
