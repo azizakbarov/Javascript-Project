359d007ba1f0c5f679bec9a8c52fb4343972f44d
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-24 09:59:54 +0500

    reduce method

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,37 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// const scores = [10, 20, 60, 40, 70, 90, 30];
 
-const products = [
-  { name: "gold star", price: 20 },
-  { name: "mushroom ", price: 40 },
-  { name: "star sell", price: 30 },
-  { name: "banana skin", price: 50 },
-  { name: "red shell", price: 10 },
+// const result = scores.reduce((acc, curr) => {
+//   if (curr > 50) {
+//     acc++;
+//   }
+//   return acc;
+// }, 0);
+
+// console.log(result);
+
+const scores = [
+  { name: "A", score: 30 },
+  { name: "B", score: 50 },
+  { name: "C ", score: 40 },
+  { name: "D", score: 10 },
+  { name: "A", score: 70 },
+  { name: "A", score: 30 },
+  { name: "B", score: 50 },
+  { name: "C ", score: 40 },
+  { name: "D", score: 10 },
+  { name: "A", score: 70 },
+  { name: "A", score: 30 },
+  { name: "B", score: 50 },
+  { name: "C ", score: 40 },
+  { name: "D", score: 10 },
+  { name: "A", score: 70 },
 ];
 
-const saleProducts = products.map((product) => {
-  if (product.price > 30) {
-    return { name: product.name, price: product.price / 2 };
-  } else {
-    return product;
+const ATotal = scores.reduce((acc, curr) => {
+  if (curr.name === "A") {
+    acc += curr.score;
   }
-});
+  return acc;
+}, 0);
 
-console.log(saleProducts);
+console.log(ATotal);
