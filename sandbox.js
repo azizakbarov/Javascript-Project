4b1d9fc7c5a880a79416188085e05df1d3b9671f
Aziz Akbarov <azizbakbarov@gmail.com>
2021-12-24 09:17:16 +0500

    Filter method

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,20 @@
-const form = document.querySelector(".signup-form");
-const feedback = document.querySelector(".feedback");
-const usernamePattern = /^[a-zA-Z]{6,12}$/;
+const scores = [10, 30, 15, 25, 50, 40, 5];
 
-// validation
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const username = form.username.value;
-
-  if (usernamePattern.test(username)) {
-    feedback.textContent = "that username is valid!";
-  } else {
-    feedback.textContent =
-      "username must contain only letters & be between 6 & 12 characters";
-  }
+const filteredScores = scores.filter((score) => {
+  return score > 20;
 });
 
-// live feedback
-form.username.addEventListener("keyup", (e) => {
-  // console.log(e.target.value, form.username.value);
-  if (usernamePattern.test(e.target.value)) {
-    //console.log('passed');
-    form.username.setAttribute("class", "success");
-  } else {
-    //console.log('failed');
-    form.username.setAttribute("class", "error");
-  }
+console.log(filteredScores);
+
+const users = [
+  { name: "A", premium: true },
+  { name: "B", premium: false },
+  { name: "C", premium: false },
+  { name: "D", premium: true },
+];
+
+const premiumUsers = users.filter((user) => {
+  return user.premium;
 });
+
+console.log(premiumUsers);
