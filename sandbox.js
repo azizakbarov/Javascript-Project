92e12d7d3ed4a75a28458b511c53317305498c62
Aziz Akbarov <azizbakbarov@gmail.com>
2022-01-02 10:23:28 +0500

    async & await

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,19 @@
-fetch("todos/luigi.json")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json()
-  }).then(data => {
-      console.log(data)
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json()
+//   }).then(data => {
+//       console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
+const getTodos = async () => {
+  const response = await fetch("todos/luigi.json");
+  const data = await response.json();
+  return data;
+};
+
+getTodos().then( data => {
+    console.log('resolved', data)
+});
